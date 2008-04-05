705cb2fd95fa4e9ebe978fc05b29843c44b3ea54
Jack Lange <jarusl@cs.northwestern.edu>
2008-04-05 01:20:19 +0000

    *** empty log message ***

--- palacios/include/palacios/vmm_types.h
+++ palacios/include/palacios/vmm_types.h
@@ -4,19 +4,19 @@
 #include <geekos/ktypes.h>
 
 /*
-
-typedef signed char s8;
+typedef signed char schar_t;
 typedef unsigned char uchar_t;
 
-typedef signed short s16;
+typedef signed short sshort_t;
 typedef unsigned short ushort_t;
 
-typedef signed int s32;
+typedef signed int sint_t;
 typedef unsigned int uint_t;
 
-typedef signed long long s64;
+typedef signed long long sllong_t;
 typedef unsigned long long ullong_t;
 
+typedef signed long slong_t;
 typedef unsigned long ulong_t;
 
 typedef unsigned long size_t;
