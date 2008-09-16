844195762a89c83e0ee4d5e8ba070d36470b6a4a
Jack Lange <jarusl@cs.northwestern.edu>
2008-09-16 19:25:03 +0000

    added network subsystem

---
+++ palacios/include/geekos/net.h
@@ -0,0 +1,15 @@
+#ifndef __NET_H__
+#define __NET_H__
+
+
+
+void Init_Network();
+
+
+void test_network();
+
+
+
+
+
+#endif
---
+++ palacios/src/geekos/net.c
@@ -0,0 +1,32 @@
+#include <geekos/net.h>
+#include <geekos/socket.h>
+#include <geekos/ne2k.h>
+
+void Init_Network() {
+  init_socket_layer();
+}
+
+
+
+
+void test_network() {
+
+    uchar_t local_addr[4];
+    uchar_t remote_addr[4];
+
+    local_addr[0] = 10;
+    local_addr[1] = 0;
+    local_addr[2] = 2;
+    local_addr[3] = 21;
+
+    set_ip_addr(local_addr);
+
+    remote_addr[0] = 10;
+    remote_addr[1] = 0;
+    remote_addr[2] = 2;
+    remote_addr[3] = 20;
+
+
+    connect(remote_addr, 4301);
+
+}
