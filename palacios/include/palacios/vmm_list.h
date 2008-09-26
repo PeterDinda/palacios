abffcb058993b19404622ce45cdce3b03045df54
Jack Lange <jarusl@cs.northwestern.edu>
2008-09-25 22:14:03 -0500

    one of these days I'll commit without breaking the build...

--- palacios/include/palacios/vmm_list.h
+++ palacios/include/palacios/vmm_list.h
@@ -1,4 +1,4 @@
-/* Stolen From Linux list implementation*/
+/* Stolen From Linux list implementation */
 /* Modifications by Jack Lange <jarusl@cs.northwestern.edu> */
 /* (c) 2008, Jack Lange <jarusl@cs.northwestern.edu> */
 /* (c) 2008, The V3VEE Project <http://www.v3vee.org> */
