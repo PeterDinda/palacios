86d4afa4bb4bb68cb4a65d562c0bdcbdb71c201a
Jack Lange <jarusl@cs.northwestern.edu>
2008-09-25 22:14:03 -0500

    one of these days I'll commit without breaking the build...

--- palacios/src/geekos/socket.c
+++ palacios/src/geekos/socket.c
@@ -1,5 +1,5 @@
 /* (c) 2008, Jack Lange <jarusl@cs.northwestern.edu> */
-/* (c) 2008, Lei Xia <xiaxlei@gmail.com>
+/* (c) 2008, Lei Xia <xiaxlei@gmail.com> */
 /* (c) 2008, The V3VEE Project <http://www.v3vee.org> */
 
 #include <geekos/socket.h>
