df9b4872e6aeed3d7f50a33c5447ea9dec83fcfa
Peter Dinda <pdinda@northwestern.edu>
2008-12-04 12:14:18 -0600

    Removed make world from this script

--- build/CreateIso.sh
+++ build/CreateIso.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-make DEBUG_ALL=1 world
+#make DEBUG_ALL=1 world
 cp vmm.img iso
 mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o test.iso iso
 
