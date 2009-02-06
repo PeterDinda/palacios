671625bf466a571d4db5c452004197a0488cee65
Jack Lange <jarusl@cs.northwestern.edu>
2009-02-06 00:18:00 -0600

    added license

--- palacios/src/palacios/vmm_shadow_paging_64.h
+++ palacios/src/palacios/vmm_shadow_paging_64.h
@@ -1,3 +1,22 @@
+/* 
+ * This file is part of the Palacios Virtual Machine Monitor developed
+ * by the V3VEE Project with funding from the United States National 
+ * Science Foundation and the Department of Energy.  
+ *
+ * The V3VEE Project is a joint project between Northwestern University
+ * and the University of New Mexico.  You can find out more at 
+ * http://www.v3vee.org
+ *
+ * Copyright (c) 2008, Jack Lange <jarusl@cs.northwestern.edu> 
+ * Copyright (c) 2008, The V3VEE Project <http://www.v3vee.org> 
+ * All rights reserved.
+ *
+ * Author: Jack Lange <jarusl@cs.northwestern.edu>
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "V3VEE_LICENSE".
+ */
+
 
 static inline int activate_shadow_pt_64(struct guest_info * info) {
   struct cr3_64 * shadow_cr3 = (struct cr3_64 *)&(info->ctrl_regs.cr3);
@@ -268,8 +287,8 @@ static int handle_pde_shadow_pagefault_64(struct guest_info * info, addr_t fault
     ((pde64_2MB_t *)guest_pde)->dirty = 1;
     shadow_pde->writable = guest_pde->writable;
 
-    PrintDebug("Returning due to large page Write Error\n");
-    PrintHostPageTree(info, fault_addr, info->ctrl_regs.cr3);
+    //PrintDebug("Returning due to large page Write Error\n");
+    //PrintHostPageTree(info, fault_addr, info->ctrl_regs.cr3);
 
     return 0;
   } else if ((shadow_pde_access != PT_ACCESS_NOT_PRESENT) &&
