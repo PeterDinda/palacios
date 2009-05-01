54422ed2a539195fca95bc065627d38f0dfbda34
Jack Lange <jarusl@cs.northwestern.edu>
2009-05-01 14:44:38 -0500

    removed superfulous build documentation

--- build/RunPuppy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ../geekos/build/vmm.img ./
-/usr/local/qemu/bin/qemu-system-x86_64   -serial file:serial.out -m 1024 -fda vmm.img -cdrom /opt/vmm-tools/isos/puppy.iso
--- build/RunXP.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/qemu/bin/qemu-system-x86_64   -serial file:serial.out -m 1024 -fda vmm.img -cdrom /opt/vmm-tools/isos/winxp.iso
