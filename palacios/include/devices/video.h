701c70aa5e0c3122fb51e1b0f524d6e6b7be5e74
Steven Jaconette <jacones@u.northwestern.edu>
2009-08-17 14:48:41 -0500

    Virtual Console

---
+++ palacios/include/devices/video.h
@@ -0,0 +1,41 @@
+/* 
+ * This file is part of the Palacios Virtual Machine Monitor developed
+ * by the V3VEE Project with funding from the United States National 
+ * Science Foundation and the Department of Energy.  
+ *
+ * The V3VEE Project is a joint project between Northwestern University
+ * and the University of New Mexico.  You can find out more at 
+ * http://www.v3vee.org
+ *
+ * Copyright (c) 2009, Robert Deloatch <rtdeloatch@gmail.com>
+ * Copyright (c) 2009, Steven Jaconette <stevenjaconette2007@u.northwestern.edu> 
+ * Copyright (c) 2009, The V3VEE Project <http://www.v3vee.org> 
+ * All rights reserved.
+ *
+ * Author: Robdert Deloatch <rtdeloatch@gmail.com>
+ *         Steven Jaconette <stevenjaconette2007@u.northwestern.edu>
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "V3VEE_LICENSE".
+ */
+
+#ifndef __DEVICES_VIDEO_H__
+#define __DEVICES_VIDEO_H__
+
+#ifdef __V3VEE__
+
+
+//#include <palacios/vm_dev.h>
+
+#include <palacios/vmm_dev_mgr.h>
+
+//struct vm_device * v3_create_video();
+
+struct video_cfg {
+  char pci[32];
+};
+
+
+#endif // ! __V3VEE__
+
+#endif
