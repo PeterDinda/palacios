7d80144eeb8dc7f5bb48975a7d7ab0d3b126d795
Kyle Hale <kh@u.northwestern.edu>
2011-06-20 18:49:12 -0400

    Software interrupt decoder support

--- palacios/include/palacios/vmm_instr_decoder.h
+++ palacios/include/palacios/vmm_instr_decoder.h
@@ -28,6 +28,7 @@ typedef enum {
     SMSW,
     CLTS,
     INVLPG,
+    INT, 
 
     MOV_CR2,
     MOV_2CR,
@@ -310,6 +311,7 @@ static int get_operand_width(struct guest_info * info, struct x86_instr * instr,
 		    return -1;
 	    }
 
+	case INT:
 	case MOV_DR2:
 	case MOV_2DR:
 	case MOV_CR2:
@@ -1331,6 +1333,8 @@ static op_form_t op_code_to_form(uint8_t * instr, int * length) {
 	case 0xf4:
 	    return HLT;
 
+	case 0xcd:
+		return INT;
 
 	case 0xf6: {
 	    struct modrm_byte * modrm = (struct modrm_byte *)&(instr[1]);
@@ -1493,6 +1497,7 @@ static char * op_form_to_str(op_form_t form) {
 	case SETO: return "SETO";
 	case STOS_8: return "STOS_8";
 	case STOS: return "STOS";
+	case INT: return "INT";
 
 	case INVALID_INSTR:
 	default:
